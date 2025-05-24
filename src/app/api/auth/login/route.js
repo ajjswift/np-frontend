6079fcd12feee6f7f2ee96329b3af92b6de743c4
Alex Swift <alex.jones@jellybaby.com>
2025-05-24 21:05:26 +0100

    Push cookie store change

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -78,6 +78,7 @@ export async function POST(req) {
             sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             path: "/",
             expires: expiresAt,
+            domain: ".ajjs.co.uk",
         });
 
         return response;
